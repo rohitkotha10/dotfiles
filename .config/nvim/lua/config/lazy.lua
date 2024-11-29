5ed209ba8ee188b0e381d4cf3fe929e0e31c52a2
Rohit Kotha <rohitkotha10@gmail.com>
2024-11-29 23:06:43 +0530

    catppuccin and nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -26,24 +26,42 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     {
-      "folke/tokyonight.nvim",
-      lazy = false,
-      priority = 1000,
-      opts = {},
+      "catppuccin/nvim",
+      name = "catppuccin",
+      lazy = false, -- make sure we load this during startup if it is your main colorscheme
+      priority = 1000, -- make sure to load this before all the other start plugins
+      config = function()
+        -- load the colorscheme here
+        vim.cmd.colorscheme "catppuccin-mocha"
+      end
     },
     {
       "nvim-telescope/telescope.nvim", 
       tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      config = function()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set('n', '<leader><leader>', builtin.oldfiles, { desc = 'Telescope previously open files' })
+      end
     },
     {
       "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate"
+      build = ":TSUpdate",
+      config = function ()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+          auto_install = true,
+          highlight = { enable = true },
+          indent = {enable = true },
+        })
+      end
     }
   },
   -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "tokyonight" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
