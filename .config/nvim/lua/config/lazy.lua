bada816c60953f3172e28c56d21a561e183b46af
Rohit Kotha <rohitkotha10@gmail.com>
2024-11-29 23:37:40 +0530

    neovim config

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -59,6 +59,23 @@ require("lazy").setup({
           indent = {enable = true },
         })
       end
+    },
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+      }
+    },
+    {
+      'nvim-lualine/lualine.nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      config = function() 
+        require('lualine').setup({options = { theme = "dracula" }})
+      end
     }
   },
   -- Configure any other settings here. See the documentation for more details.
